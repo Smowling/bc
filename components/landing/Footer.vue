6a039faccb79221fdbda99b9117cd25a6b8ec16b
Daniel Smółka <smowling@gmail.com>
2024-03-07 18:37:18 +0100

    cp

--- components/landing/Footer.vue
+++ components/landing/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="my-20">
     <p class="text-center text-sm text-slate-500">
-      Copyright © {{ new Date().getFullYear() }} <span class="font-bold text-slate-800">Bike</span
+      Copyright <span class="font-bold text-slate-800">Bike</span
           ><span class="text-slate-500">check</span
-          ><span class="font-bold text-slate-800">!</span> All rights reserved.
+          ><span class="font-bold text-slate-800">!</span> © {{ new Date().getFullYear() }}  All rights reserved.
     </p>
   </footer>
 </template>
